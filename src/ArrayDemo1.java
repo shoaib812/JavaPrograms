fcfd4ac8e9a41b64d63310c9bec0c8a47db9b083
shoaib812 <Shoaib@78400>
2022-10-04 11:09:58 +0530

    git commit -m "shoaib812/JavaPrograms/ArrayDemo1"

--- src/ArrayDemo1.java
+++ src/ArrayDemo1.java
@@ -1,2 +1,33 @@
-public class ArrayDemo1 {
+public class ArrayDemo1
+{
+    public static void main(String[]ar)
+    {
+        int arr[] = new int[5];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+        System.out.println();
+        System.out.println();
+
+        arr[0] = 11;
+        arr[1] = 22;
+        arr[2] = 33;
+        arr[3] = 44;
+        arr[4] = 55;
+
+        for(int i=0; i<arr.length; i++)
+        {
+
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+        System.out.println();
+          for(int x : arr)
+          {
+              System.out.println(x);
+          }
+    }
 }
